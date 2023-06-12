46e29c3f3fc2a9a854cabd8b3515969ea94aba17
Arthur Daleprani <102961025+arthurdaleprani@users.noreply.github.com>
2023-06-12 10:55:15 -0300

    Add files via upload

---
+++ src/components/pages/dashbord/index.js
@@ -0,0 +1,10 @@
+import { Text, View} from "react-native";
+
+
+export default function dashbord({ }) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.Esqtxt}>dashbord</Text>
+      </View>
+    );
+  }
